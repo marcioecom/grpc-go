4f5453f17b263113b4ac62852edde0b8d04f1cde
Marcio Junior <marcio.mj70@gmail.com>
2022-07-25 20:12:39 -0300

    feat: upvote crypto

--- server/main.go
+++ server/main.go
@@ -3,9 +3,11 @@ package server
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/marcioecom/grpc-go/database/models"
 	"github.com/marcioecom/grpc-go/pb"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"google.golang.org/grpc/codes"
@@ -45,4 +47,26 @@ func (s *server) CreateCrypto(ctx context.Context, req *pb.Crypto) (*pb.CreateCr
 	oid := result.InsertedID.(primitive.ObjectID)
 	req.Id = oid.Hex()
 	return &pb.CreateCryptoResponse{Crypto: req}, nil
+}
+
+func (s *server) UpvoteCrypto(ctx context.Context, req *pb.UpvoteCryptoRequest) (*pb.UpvoteCryptoResponse, error) {
+	var crypto models.Crypto
+
+	oid, err := primitive.ObjectIDFromHex(req.GetId())
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, fmt.Sprintf("Invalid ObjectId: %v", err.Error()))
+	}
+
+	filter := bson.M{"_id": oid}
+	update := bson.M{"$inc": bson.M{"up": 1, "total": 1}}
+
+	err = s.collection.FindOneAndUpdate(s.mongoCtx, filter, update).Decode(&crypto)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, status.Errorf(codes.NotFound, "Crypto not found")
+		}
+		log.Fatal(err)
+	}
+
+	return &pb.UpvoteCryptoResponse{Ok: true}, nil
 }
